69a751dade3a634458d438dd55cd4f15d55a1dd5
Mustafa Hasturk <mustafa.hasturk@yandex.com>
2016-02-09 05:02:36 +0200

    fix gruntfile path

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,9 +31,9 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'images/',
+          cwd: 'assets/images/',
           src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
+          dest: 'assets/images/'
         }]
       }
     },
@@ -41,9 +41,9 @@ module.exports = function(grunt) {
       dist: {
         files: [{
           expand: true,
-          cwd: 'images/',
+          cwd: 'assets/images/',
           src: '{,*/}*.svg',
-          dest: 'images/'
+          dest: 'assets/images/'
         }]
       }
     },
